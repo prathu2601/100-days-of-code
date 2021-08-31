985a2a34f995c8ab1de61c9b0e3c54e70e677618
prathu2601 <patupmpmdhampu@gmail.com>
2021-08-31 23:46:36 +0530

    day 35

---
+++ Day 35/Domino on Windowsill.cpp
@@ -0,0 +1,47 @@
+//Link to question
+//"https://codeforces.com/problemset/problem/1499/A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n, a, b, c, d;
+        cin >> n >> a >> b >> c >> d;
+        c = 2*c - (a+b);
+        d = 2*d - (2*n-(a+b));
+        //cout << c << ' ' << d << ' ';
+        if(c > 0 or d > 0)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+    }
+    return 0;
+}
---
+++ Day 35/Déjà Vu.cpp
@@ -0,0 +1,64 @@
+//Link to question
+//"https://codeforces.com/problemset/problem/1504/A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        string s, c, a;
+        cin >> s;
+        c = 'a' + s;
+        s += 'a';
+        ll flag = 0;
+        n = s.size();
+        F0r(i, n/2){
+            if(s[i] != s[n-i-1]){
+                a = s;
+                flag = 1;
+                break;
+            }
+        }
+        F0r(i, n/2){
+            if(c[i] != c[n-i-1]){
+                a = c;
+                flag = 1;
+                break;
+            }
+        }
+        if(!flag)
+            cout << "NO\n";
+        else{
+            cout << "YES\n" << a << '\n';
+        }
+    }
+    return 0;
+}
