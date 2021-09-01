3482b08189d6902d7a5167245047a88dcd901044
prathu2601 <patupmpmdhampu@gmail.com>
2021-09-02 00:30:09 +0530

    day 36

---
+++ Day 36/Anti-knapsack.cpp
@@ -0,0 +1,47 @@
+//Link to question
+//"https://codeforces.com/problemset/problem/1493/A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n, k;
+        cin >> n >> k;
+        set <ll> st;
+        For(i, (k+1)/2, k) st.insert(i);
+        For(i, k+1, n+1) st.insert(i);
+        cout << st.size() << '\n';
+        FOR(it, st) cout << it << ' ';
+        if(st.size() > 0)
+            cout << '\n';
+    }
+    return 0;
+}
