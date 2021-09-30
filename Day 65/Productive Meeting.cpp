e153f232f6e823870c9fdffe63a74500ae863d40
prathu2601 <patupmpmdhampu@gmail.com>
2021-09-30 23:52:30 +0530

    day 65

---
+++ Day 65/Productive Meeting.cpp
@@ -0,0 +1,58 @@
+//Link to question
+//"https://codeforces.com/contest/1579/problem/D"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define sz(a) (a).size()
+#define fi first
+#define se second
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        ll a[n];
+        priority_queue<pair<ll, ll> > pq;
+        vector<pair<ll, ll> > ans;
+        F0r(i, n){
+            cin >> a[i];
+            if(a[i]!=0)
+                pq.push(mp(a[i], i+1));
+        }
+        while(sz(pq) > 1){
+            auto [x, y] = pq.top();pq.pop();
+            auto [z, w] = pq.top();pq.pop();
+            ans.pb(mp(y, w));
+            if(x > 1) pq.push(mp(x-1, y));
+            if(z > 1) pq.push(mp(z-1, w));
+        }
+        cout << sz(ans) << '\n';
+        FOR(it, ans) cout << it.fi << ' ' << it.se << '\n';
+    }
+    return 0;
+}
---
+++ Day 65/Ticks.cpp
@@ -0,0 +1,70 @@
+//Link to question
+//"https://codeforces.com/contest/1579/problem/C"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define sz(a) (s).size()
+#define fi first
+#define se second
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n, m, k;
+        cin >> n >> m >> k;
+        vector<string> vs(n), a(n);
+        string s(m, '.');
+        F0r(i, n) cin >> vs[i], a[i] = s;
+        F0r(i, n){
+            F0r(j, m){
+                if(vs[i][j] == '*'){
+                    ll x = 1;
+                    while(1){
+                        if(i-x < 0 or j-x < 0 or vs[i-x][j-x]!='*')break;
+                        if(i-x < 0 or j+x < 0 or vs[i-x][j+x]!='*')break;
+                        x++;
+                    }
+                    x--;
+                    if(x >= k){
+                        F0r(z, x+1) a[i-z][j-z] = a[i-z][j+z] = '*';
+                    }
+                }
+            }
+        }
+        ll flag = 0;
+        F0r(i, n){
+            F0r(j, m){
+                if(vs[i][j] != a[i][j]){
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        cout << (flag == 1?"NO":"YES") << '\n';
+    }
+    return 0;
+}
