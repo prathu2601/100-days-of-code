ca485e0501d09de8214e9f2d27ca151fe34dd079
prathu2601 <patupmpmdhampu@gmail.com>
2021-10-30 16:16:19 +0530

    day 90

---
+++ Day 90/Windblume Ode.cpp
@@ -0,0 +1,67 @@
+//Link to question
+//"https://codeforces.com/problemset/problem/1583/A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define sz(a) (a).size()
+#define fi first
+#define se second
+#define vl vector<ll>
+#define vll vector<vl>
+#define pl pair<ll,ll>
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        vl a(n);
+        ll sum = 0;
+        F0r(i, n) cin >> a[i], sum += a[i];
+        ll flag = 0;
+        for(ll i=3; i*i<= sum; i+=2){
+            if(sum%i == 0) flag = 1;
+        }
+        if(sum%2 == 0 or flag){
+            cout << n << '\n';
+            F0r(i, n) cout << i+1 << ' ';
+        }
+        else{
+            cout << n-1 << '\n';
+            F0r(i, n){
+                if(a[i]%2 and !flag){
+                    flag = 1;
+                }
+                else
+                    cout << i+1 << ' ';
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
