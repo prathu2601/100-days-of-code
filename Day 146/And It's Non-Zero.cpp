b47cd3805621cc8dfc0b71e8034118bbbf19c7e2
prathu2601 <patupmpmdhampu@gmail.com>
2021-12-26 15:26:06 +0530

    day 147

---
+++ Day 146/And It's Non-Zero.cpp
@@ -0,0 +1,57 @@
+//Link to question
+//"https://codeforces.com/contest/1615/problem/B"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define sz(a) (a).size()
+#define con(a) {cout << a << '\n';continue;}
+#define bre(a) {cout << a << '\n';break;}
+#define fi first
+#define se second
+#define vl vector<ll>
+#define vll vector<vl>
+#define pl pair<ll,ll>
+#define YN(flag) cout << (flag?"YES":"NO") << '\n';
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    vll an(20, vl(200001, 0));
+    F0r(i, 20){
+        For(j, 1, 200001){
+            an[i][j] = an[i][j-1]+((j>>i)&1);
+        }
+    }
+    while(t--){
+        ll l, r;
+        cin >> l >> r;
+        ll ans = r-l+1;
+        F0r(i, 20) ans = min(ans, r-l+1-an[i][r]+an[i][l-1]);
+        cout << ans << '\n';
+    }
+    return 0;
+}
