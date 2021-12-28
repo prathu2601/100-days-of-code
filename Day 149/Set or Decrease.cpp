ea341590a2203ecf780a1cbc3207587aa854a049
prathu2601 <patupmpmdhampu@gmail.com>
2021-12-28 22:56:49 +0530

    day 149

---
+++ Day 149/Set or Decrease.cpp
@@ -0,0 +1,61 @@
+//Link to question
+//"https://codeforces.com/contest/1622/problem/C"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define For(i, a, b) for(ll i=a; i<(b); i++)
+#define F0r(i, a) for(ll i=0; i<(a); i++)
+#define Forr(i, a, b) for(ll i = (b)-1; i >= a; i--)
+#define F0rr(i, a) for(ll i = (a)-1; i >= 0; i--)
+#define FOR(it, a) for(auto it:(a))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define max(a, b) ((a > b)?(a):(b))
+#define min(a, b) ((a < b)?(a):(b))
+#define pb(x) push_back((x))
+#define mp(x, y) make_pair(x, y)
+#define sz(a) (a).size()
+#define con(a) {cout << a << '\n';continue;}
+#define bre(a) {cout << a << '\n';break;}
+#define fi first
+#define se second
+#define vl vector<ll>
+#define vll vector<vl>
+#define pl pair<ll,ll>
+#define YN(flag) cout << (flag?"YES":"NO") << '\n';
+#define MOD 1000000007
+#define DOM 998244353
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    while(t--){
+        ll n, k;
+        cin >> n >> k;
+        vl a(n), b(n+1);
+        F0r(i, n) cin >> a[i];
+        sort(all(a));
+        F0r(i, n) b[i+1] = b[i]+a[i];
+        ll ans = 9LL*MOD*MOD;
+        F0r(i, n){
+            ll sum = b[n-i]+a[0]*i, an = i;
+            if(sum > k){
+                an += (sum-k+i)/(i+1);
+            }
+            ans = min(ans, an);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
